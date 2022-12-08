78bdab58f3a5af37d225899867b4af938089cf42
Chris Choute <christopherchoute@gmail.com>
2022-12-07 22:18:05 -0500

    commit tree f64eebf68df8bf7d8c1e24206f6b2c7efe955a29

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    return cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    const appendCat = [...cats, "Broom"];
+    return appendCat;
+}
+
+function prependCat(name) {
+    const prependCat = ["Arnold",...cats];
+    return prependCat;
+}
+
+function removeLastCat(name) {
+    const removeLastCat = cats.slice();
+    return removeLastCat.slice(0,2);
+}
+
+function removeFirstCat() {
+    const removeFirstCat = cats.slice();
+    return removeFirstCat.slice(1);
+}
